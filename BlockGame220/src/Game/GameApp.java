e2aaab2b0fc5a81147c46586d9c0b3f117089294
barczearROSE <barczear@rose-hulman.edu>
2025-10-30 15:35:33 -0400

    Restructured GameApp and GamePanel, started programming the window

---
+++ BlockGame220/src/Game/GameApp.java
@@ -0,0 +1,22 @@
+package Game;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+
+public class GameApp{
+
+	private JFrame frame = new JFrame();
+	private GamePanel panel = new GamePanel();
+	
+	//Constructs the main game window.
+	public  GameApp()
+	{
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(panel);
+        frame.pack();                      // Fit to preferred component sizes
+        frame.setLocationRelativeTo(null); // Center on screen
+		
+	}
+}
--- BlockGame220/src/Game/GamePanel.java
+++ BlockGame220/src/Game/GamePanel.java
@@ -11,54 +11,23 @@ public class GamePanel extends JPanel{
 	private JButton endGameButton = new JButton("End Game");
 	
 	public GamePanel() {
+			
 		   setLayout(new FlowLayout());
 		   scoreLabel.setFont(scoreLabel.getFont().deriveFont(24f));
 		   add(scoreLabel);
 		   add(addPointButton);
 		   add(endGameButton);
 		   
-		   
-		  addPointButton.addActionListener(e -> addScores());
-		  endGameButton.addActionListener(e -> onEndGame());
+		  
 		}
 	
-	private void addScores() {
-        score++;
-        scoreLabel.setText("Score: " + score);
-      }
-
-	private void onEndGame() {
-        //String player = JOptionPane.showInputDialog(this, "Enter your name", "Player");
-        //if (player == null || player.isBlank()) player = "Anonymous";
-      
-        //ScoreManager.saveScore(player, score);
-        //showLeaderboard();
-      
-        score = 0;
-        scoreLabel.setText("Score: 0");
-      }
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		
+	}
 	
-	/*
-	private void showLeaderboard() {
-        List lines = ScoreManager.loadScores();
-          StringBuilder html = new StringBuilder(
-            "<html><h2>Leaderboard</h2><ol>"
-        );
-        for (int i = 0; i < lines.size() && i < 5; i++) {
-            String[] parts = lines.get(i).split(",", 2);
-            html.append("<li>")
-                .append(parts[0])    // player name
-                .append(": ")
-                .append(parts[1])    // score
-                .append("</li>");
-        }
-        html.append("</ol></html>");
-      
-        JOptionPane.showMessageDialog(
-          this,
-          new JLabel(html.toString()),
-          "Top Scores",
-          JOptionPane.PLAIN_MESSAGE
-        );
-      } */
 }
