754da223893c370fd9e9e5a5843475973018b6fa
barczearROSE <barczear@rose-hulman.edu>
2025-10-30 16:08:00 -0400

    Created HUD file

---
+++ BlockGame220/src/Game/HUDModel.java
@@ -0,0 +1,21 @@
+package Game;
+
+public class HUDModel {
+	
+	private int score = 0;
+	
+	
+	//Returns score variable.
+	public int getScore()
+	{
+		return score;
+	}
+	
+	//Increment Score
+	public void incScore()
+	{
+		this.score += 1;
+	}
+}
+
+
