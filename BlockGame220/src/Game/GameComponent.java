bcefe11e160ea78542f3b35c8226b3373f640c8c
barczearROSE <barczear@rose-hulman.edu>
2025-10-30 15:51:30 -0400

    Working further on GameApp, Component, Panel

---
+++ BlockGame220/src/Game/GameComponent.java
@@ -0,0 +1,14 @@
+package Game;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+import javax.swing.Timer;
+
+public class GameComponent extends JComponent{
+
+	
+}
--- BlockGame220/src/Game/GamePanel.java
+++ BlockGame220/src/Game/GamePanel.java
@@ -3,23 +3,30 @@ package Game;
 import java.awt.*;
 import java.util.List;
 import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
 
 public class GamePanel extends JPanel{
+	
+	private final GameComponent canvas = new GameComponent();
 	private int score = 0;
 	private JLabel scoreLabel = new JLabel("Score: 0");
-	private JButton addPointButton = new JButton("Add Point");
-	private JButton endGameButton = new JButton("End Game");
+	Timer timer;
 	
 	public GamePanel() {
-			
 		   setLayout(new FlowLayout());
 		   scoreLabel.setFont(scoreLabel.getFont().deriveFont(24f));
 		   add(scoreLabel);
-		   add(addPointButton);
-		   add(endGameButton);
-		   
 		  
 		}
+	
+	
 	
 	@Override
 	protected void paintComponent(Graphics g)
@@ -28,6 +35,23 @@ public class GamePanel extends JPanel{
 		Graphics2D g2 = (Graphics2D)g;
 		
 		
+	}
+	
+	
+	
+	private JComponent buildControls()
+	{
+		JPanel controls = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 6));
+		
+        JButton left = new JButton("Left");
+        JButton right = new JButton("Right");
+        JButton up = new JButton("Up");
+        
+        left
+        
+        
+        
+		return 
 	}
 	
 }
