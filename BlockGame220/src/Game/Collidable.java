a3fc8dc810baf09c13d6862b6c298c292b9209e3
connorhp <connorhp@RHIT-PW0HMCBT.rose-hulman.edu>
2025-10-30 16:41:29 -0400

    Added getters, setters, and a constructor for x-position, y-position, and velocity.

--- BlockGame220/src/Game/Collidable.java
+++ BlockGame220/src/Game/Collidable.java
@@ -4,7 +4,39 @@ import java.awt.Graphics2D;
 
 public class Collidable {
 	
+	private int X;
+	private int Y;
+	private int V;
 	
+	public Collidable(int X, int Y, int V) {
+		this.X = X;
+		this.Y = Y;
+		this.V = V;
+	}
+	
+	public int getX() {
+		return X;
+	};
+	
+	public int getY() {
+		return Y;
+	};
+	
+	public int getV() {
+		return V;
+	};
+	
+	public void setX(int X) {
+		this.X = X;
+	};
+	
+	public void setY(int Y) {
+		this.Y = Y;
+	};
+	
+	public void setV(int V) {
+		this.V = V;
+	};
 	
 	public void draw(Graphics2D g2) {
 		
