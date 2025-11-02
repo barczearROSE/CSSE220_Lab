2563faf74021a054f94ace8d65e2a043177f35fa
connorhp <connorhp@RHIT-PW0HMCBT.rose-hulman.edu>
2025-11-02 18:31:30 -0500

    hello

--- BlockGame220/src/Game/Enemy.java
+++ BlockGame220/src/Game/Enemy.java
@@ -1,18 +1,50 @@
 package Game;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Enemy extends Collidable {
 
-	int reverse = 1;
+	private BufferedImage sprite;
+	private boolean spriteLoaded = false;
+	private int maxDist;
+	private int minDist;
+	private int speed = 10;
 	
-	public Enemy(int X, int Y) {
+	public Enemy(int X, int Y, int maxDist, int minDist) {
 		super(X, Y);
+		this.maxDist = maxDist;
+		this.minDist = minDist;
+		try {
+            sprite = ImageIO.read(Enemy.class.getResource("emeny.png"));
+            spriteLoaded = (sprite != null);
+        } catch (IOException | IllegalArgumentException ex) {
+            spriteLoaded = false; // fallback to oval
+        }
 	}
 	
-	public void move(int maxDist, int minDist, int speed) {
-		if (X + speed >= maxDist || X - speed >= minDist) {
-			reverse = reverse * -1;
+	public void move() {
+		if (X + speed >= maxDist || X - speed <= minDist) {
+//			if (reverse < 0) {
+//				this.X = maxDist;
+//			} else if (reverse > 0) {
+//				this.X = minDist;
+//			};
+			speed = speed * -1;
 		};
-		this.X += speed * reverse;
+		this.X += speed;
 	};
+	
+	@Override
+	public void draw(Graphics2D g2) {
+        if (spriteLoaded) {   
+    	    g2.drawImage(sprite, X, Y, 10, 10, null);
+    	}
+	}
 
 }
+
